49195feb1c385597fe62384cdf1e48414d3c0035
iamvinaysaini <iamvinaysaini08@gmail.com>
2024-08-25 14:45:48 +0000

    Till Objects

---
+++ .devcontainer/javascript/tut1.js
@@ -0,0 +1,55 @@
+
+let ID = 53733
+let email = "audit.modinagar@fusionmicrofinance.com"
+var accountPasw = "Vinar@08"
+accountCity = "modinagar"
+console.log(email, accountPasw)
+console.table([ID , email, accountPasw, accountCity])
+console.log(email)
+
+let valueInString = String(ID)
+console.log(typeof(valueInString))
+console.log(valueInString)
+console.log("1"+(2+2))
+console.log(1+"2"+"2")
+console.log("2"+1+"2"+2)
+let gc=4
+let c=gc++
+console.log(c,gc)
+let gc1=4
+let c1=++gc1
+console.log(c1,gc1)
+console.log(2>1)
+let a=2+1+"2"
+console.log(a)
+let b ;
+console.log(typeof b)
+const funT={
+    nm:"abc",
+    ab: 123,
+}
+let bn=funT
+bn.nm="bbc"
+console.log(bn,funT.ab)
+// let iddd=11222
+// ui=636436436
+// iddd=ui
+// console.log(iddd)
+let s= "  sus    ha nt"
+console.log(s.trim())
+const othernum=123.96
+console.log(othernum.toPrecision(5))
+let mydate = new Date()
+console.log(mydate.toLocaleTimeString())
+console.log(mydate.toTimeString())
+console.log(mydate.toString())
+console.log(mydate)
+let day = Date.now()
+console.log(day)
+let arr1=[1,2,2]
+console.log(arr1)
+console.log(arr1.indexOf(2,2))
+let a1=arr1.join()
+console.log(a1)
+console.log(typeof a1)
+console.log(typeof arr1)
